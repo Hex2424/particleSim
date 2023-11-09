33018294ba88d87f19d28b0dce98fc4485d0f59b
Markas Vielavicius <maildetectionteam@gmail.com>
2023-11-10 00:50:19 +0200

    Changes in config file

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
  *
  * MORE INFO ABOUT THE FILE'S CONTENTS
  *
- * @copyright This file is a part of the project Iguana and is distributed under MIT license which
+ * @copyright This file is a part of the project particleSim and is distributed under MIT license which
  * should have been included with the project. If not see: https://choosealicense.com/licenses/mit/
  *
  * @author Markas Vielavičius (markas.vielavicius@bytewall.com)
@@ -14,9 +14,21 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-    #define ARENA_X                 500
-    #define ARENA_Y                 500
+    #define WINDOW_NAME             "Particle Life"
+    #define WINDOW_POSX             1000
+    #define WINDOW_POSY             100
 
+    #define WINDOW_W                500
+    #define WINDOW_H                500
 
-#endif // CONFIG_H_
+    // CALCULATING MOST OPTIMAL 
+    // #define ARENA_X                 WINDOW_W * PARTICLE_SIZE
+    // #define ARENA_Y                 WINDOW_H * PARTICLE_SIZE
 
+    #define PARTICLE_SIZE           5
+
+    // #if ARENA_X % PARTICLE_SIZE != 0 || ARENA_Y % PARTICLE_SIZE != 0 
+    //     #error PARTICLE SIZE IS NOT DIVISABLE PROPERLY
+    // #endif
+
+#endif // CONFIG_H_4
