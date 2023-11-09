0ad2d437ddbb6e9db707aaeb46085658098a57c6
Markas Vielavicius <maildetectionteam@gmail.com>
2023-11-10 00:49:29 +0200

    Licence updated mistake

--- physics.c
+++ physics.c
@@ -1,7 +1,26 @@
+/**
+ * @file physics.c
+ *
+ * MORE INFO ABOUT THE FILE'S CONTENTS
+ *
+ * @copyright This file is a part of the project particleSim and is distributed under MIT license which
+ * should have been included with the project. If not see: https://choosealicense.com/licenses/mit/
+ *
+ * @author Markas Vielavičius (markas.vielavicius@bytewall.com)
+ *
+ * @date 2023-11-09
+ */
+
 #include "physics.h"
 
 
 PhysicalProperties_t Physics_physic(const EnabledPhysics_t affectingPhysics, const Mass_t mass)
+{
+    const PhysicalProperties_t physicalProperties = { affectingPhysics, mass };
+    return physicalProperties;
+}
+
+PhysicalProperties_t Physics_updatePhysics(const EnabledPhysics_t affectingPhysics, const Mass_t mass)
 {
     const PhysicalProperties_t physicalProperties = { affectingPhysics, mass };
     return physicalProperties;
