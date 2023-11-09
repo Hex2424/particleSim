15fc9e3a83ecc87de13dd5bd476818a2a2e992d1
Markas Vielavicius <maildetectionteam@gmail.com>
2023-11-10 00:50:08 +0200

    updated compilation bash script

--- cmplscript.sh
+++ cmplscript.sh
@@ -4,8 +4,10 @@ if [ "$1" == "run" ]; then
     # Execute program
     ./prtLife.out
 else
+    # remove old program
+    rm ./prtLife.out
     # Compile program
-    gcc main.c particle.c physics.c logger/logger.c -lGL -lGLU -lglut -lm -o prtLife.out
+    gcc main.c particle.c physics.c logger/logger.c display.c -lGL -lGLU -lglut -lm -o prtLife.out
     # Execute program
     ./prtLife.out
 fi
