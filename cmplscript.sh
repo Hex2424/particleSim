2e8257aad85f2084b6725836c57c2e4e143774f1
Markas Vielavicius <maildetectionteam@gmail.com>
2023-11-11 03:08:43 +0200

    addded morre files to compile to script

--- cmplscript.sh
+++ cmplscript.sh
@@ -7,7 +7,7 @@ else
     # remove old program
     rm ./prtLife.out
     # Compile program
-    gcc main.c particle.c physics.c logger/logger.c display.c -lGL -lGLU -lglut -lm -o prtLife.out
+    gcc main.c particle.c physics.c logger/logger.c display.c vector.c -lGL -lGLU -lglut -lm -o prtLife.out
     # Execute program
     ./prtLife.out
 fi
