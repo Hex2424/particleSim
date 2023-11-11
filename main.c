c591fa07dc6112f139d18464aa97ba6d92f49bd5
Markas Vielavicius <maildetectionteam@gmail.com>
2023-11-11 03:08:51 +0200

    Updated main

--- main.c
+++ main.c
@@ -2,12 +2,13 @@
 #include "particle.h"
 #include "config.h"
 #include "display.h"
+#include "physics.h"
 
 // #define MAX_PARTICLES_COUNT     10
 
-static void jobFunction_(void)
+static void jobFunction_(float deltaTime)
 {
-    
+    Physics_updatePhysics(deltaTime);
 }
 
 int main(int argc, char **argv)
@@ -17,9 +18,10 @@ int main(int argc, char **argv)
 
     // Prepare particles cloud
     ParticleCloud_init(5);
-    ParticleCloud_addNewGroup(20, RGB(255, 255, 255), Physics_physic(GRAVITY_FORCE, 1));
-    ParticleCloud_addNewGroup(20, RGB(255, 0, 255), Physics_physic(GRAVITY_FORCE, 1));
-    ParticleCloud_addNewGroup(20, RGB(255, 0, 0), Physics_physic(GRAVITY_FORCE, 1));
+    ParticleCloud_addNewGroup(10, RED, Physics_physic(GRAVITY_FORCE, 1000));
+    ParticleCloud_addNewGroup(1, GREEN, Physics_physic(GRAVITY_FORCE, -100000));
+    ParticleCloud_addNewGroup(1, BLUE, Physics_physic(GRAVITY_FORCE, 100000));
+    // ParticleCloud_addNewGroup(20, RGB(255, 0, 0), Physics_physic(GRAVITY_FORCE, 100000));
    
     Display_startRendering(jobFunction_);
 
