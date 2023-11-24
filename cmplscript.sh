92d694ef9d1edde9a62752ad3fb82b8f1f50db77
Markas Vielavicius <maildetectionteam@gmail.com>
2023-11-24 22:43:53 +0200

    upgraded compile script to differend modes

--- cmplscript.sh
+++ cmplscript.sh
@@ -7,10 +7,19 @@ else
     # remove old program
     rm ./prtLife.out
     # Compile program
+    compile="gcc main.c \
+        particle.c \
+        physics.c \
+        logger/logger.c \
+        display.c \
+        vector.c -lGL -lGLU -lglut -lm -o prtLife.out"
+
     if [ "$1" == "release" ]; then
-        gcc main.c particle.c physics.c logger/logger.c display.c vector.c -lGL -lGLU -lglut -lm -o prtLife.out -O3
-    else
-        gcc main.c particle.c physics.c logger/logger.c display.c vector.c -lGL -lGLU -lglut -lm -o prtLife.out -O0
+        $compile -O3 -DLINUX -DVERBOSE_LEVEL=1
+    elif [ "$1" == "debug" ]; then
+        $compile -O0 -DLINUX -DVERBOSE_LEVEL=3
+    else 
+        $compile -O0 -DLINUX -DVERBOSE_LEVEL=2
     fi
     # Execute program
     ./prtLife.out
